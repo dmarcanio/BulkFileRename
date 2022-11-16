2ad937718e0fe3ff9af63036c74e3f3aa3bf2887
dominic marcanio <dmarcanio@wdigis.com>
2022-11-16 16:23:39 -0600

    update BulkFileRename.py entry point. Remove reference to rprenamer.py (unused)

---
+++ venv/BulkFileRename.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+# TODO: VERIFY IF THIS IS THE CORRECT INTERPRETER PATH OR IF LINE ABOVE IS NECESSARY
+# -*- coding: utf-8 -*-
+# BulkFileRename.py
+
+"""This module provides the BulkFileRename entry-point script"""
+
+#import main() function from BulkFileRename / app.py which creates new Window and shows it.
+from BulkFileRename.app import main
+
+if __name__ == "__main__":
+    main()
